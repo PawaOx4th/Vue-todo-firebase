c6228fe8f55b51b64dbc2479b586faf8ba9267b9
PawaOx4th <pawaox4th@gmail.com>
2020-10-24 22:03:50 +0700

    2020-10-24 22:33

--- src/firebase.js
+++ src/firebase.js
@@ -4,21 +4,6 @@ import "firebase/auth";
 import "firebase/database"
 import "firebase/firestore";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDpABYBLw_RlvMHmae4tpQDJL_R85C41bM",
-//   authDomain: "illusion-36497.firebaseapp.com",
-//   databaseURL: "https://illusion-36497.firebaseio.com",
-//   projectId: "illusion-36497",
-//   storageBucket: "illusion-36497.appspot.com",
-//   messagingSenderId: "211142277848",
-//   appId: "1:211142277848:web:7c9356be814238480835ad",
-//   measurementId: "G-2Y5GFJ3071"
-// };
-
-// firebase.initializeApp(firebaseConfig);
-
-
 const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyDpABYBLw_RlvMHmae4tpQDJL_R85C41bM",
   authDomain: "illusion-36497.firebaseapp.com",
@@ -30,7 +15,13 @@ const firebaseApp = firebase.initializeApp({
   measurementId: "G-2Y5GFJ3071"
 })
 
+
+
+
+// Firestore Database
 const db = firebaseApp.firestore()
+
+// Firebase RealTime Database
 const rdb = firebaseApp.database()
 
 // eslint-disable-next-line import/prefer-default-export
